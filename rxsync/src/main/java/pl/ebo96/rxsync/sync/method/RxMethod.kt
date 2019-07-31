f793a14279ad1e00fe06adac9ab7b0a20b958eb5
Sebastian Bruliński <sbrulinski@9bits.pl>
2019-07-31 10:41:55 +0200

    Remove conditional register

--- rxsync/src/main/java/pl/ebo96/rxsync/sync/method/RxMethod.kt
+++ rxsync/src/main/java/pl/ebo96/rxsync/sync/method/RxMethod.kt
@@ -25,14 +25,6 @@ class RxMethod<T : Any> private constructor(val async: Boolean, private val retr
         return operation
     }
 
-    fun conditionalRegister(getPredicate: () -> RxPredicate<T>): RxMethod<T> {
-        this.operation = Flowable.fromCallable { getPredicate() }.flatMap {
-            val operation = mapToMethodResult(it.getValue())
-            prepareOperation(operation)
-        }
-        return this
-    }
-
     fun registerOperation(operation: Flowable<T>): RxMethod<T> {
         this.operation = mapToMethodResult(operation)
         this.operation = prepareOperation(this.operation)
@@ -61,19 +53,9 @@ class RxMethod<T : Any> private constructor(val async: Boolean, private val retr
      * counting done methods and final result done method are wrong by that. We can wrap result and return 'null'.
      */
     private fun mapToMethodResult(operation: Flowable<out T>): Flowable<MethodResult<out T>> {
-//        return operation.map { result ->
-//            MethodResult(this@RxMethod, result, payload, module)
-//        }
-        return Flowable.fromCallable {
-            val operationIsEmpty = operation.isEmpty.blockingGet()
-            if (operationIsEmpty) {
-                Flowable.just(MethodResult(this@RxMethod, null, payload, module))
-            } else {
-                operation.map { result ->
-                    MethodResult(this@RxMethod, result, payload, module)
-                }
-            }
-        }.flatMap { it }
+        return operation.map { result ->
+            MethodResult(this@RxMethod, result, payload, module)
+        }
     }
 
     fun doSomethingWithResult(result: (T?) -> Unit): RxMethod<T> {
